d5dc650ff4e902ff6b2adc9223838ca106c3b005
tanyajs <tannitan1616@gmail.com>
2025-11-03 10:26:04 +0000

    Add files via upload

--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -1,9 +1,55 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(2000, 2000);
+
 }
 
 function draw() {
-  background(220);
-  circle(50,50,50)
-  
+  background(168, 102, 255)
+ 
+//face
+fill(92, 0, 153)
+circle(598, 310, 515)
+
+//eyes
+fill(51, 0, 51)
+circle(500, 200, 125);
+circle(700, 200, 125)
+
+//smaller eyes
+fill(255, 255, 255)
+circle(500, 180, 55)
+circle(700, 180, 55)
+
+//ears
+fill(92, 0, 153)
+noStroke();
+circle(390, 100, 160)
+circle(810, 100, 160)
+
+//smaller ears
+fill(51, 0, 51)
+circle(390, 100, 90)
+circle(810, 100, 90)
+
+
+// mouth
+fill(224, 179, 255)
+ellipse(600, 450, 280, 160);
+
+//nose
+fill(64, 0, 128)
+square(570, 280, 55, 20);
+
+
+
+// Style the line
+fill(0, 0 ,0)
+stroke(77, 0, 102);
+strokeWeight(5);
+
+
+
+
+
+
 }
